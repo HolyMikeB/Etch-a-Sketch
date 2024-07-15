33e31594b8a55e5cb79b49a1dd07406ff6c75768
HolyMikeB <bnkrdicy@aol.com>
2024-07-14 22:13:50 -0500

    Added exception catching to reset function

--- script.js
+++ script.js
@@ -25,29 +25,34 @@ for (let i = 0; i < box.length; i++) {
 const reset = document.querySelector("button");
 
 reset.addEventListener("click", () => {
-    for (let i = 0; i < box.length; i++) {
-        container.removeChild(box[i]);
-    }
     let newBoard = Number(prompt("Please enter the width of your new board (between 1 and 100)"));
     let newBoardSize = newBoard ** 2;
+    if (newBoard > 100) {
+        alert("Please enter a number between 1 and 100.")
+    } else {
+        for (let i = 0; i < box.length; i++) {
+            container.removeChild(box[i]);
+        }
+        
 
-    for (let i = 0; i < newBoardSize; i++) {
-        const div = document.createElement("div");
-        div.setAttribute("class", "box");
-        container.appendChild(div);
+        for (let i = 0; i < newBoardSize; i++) {
+            const div = document.createElement("div");
+            div.setAttribute("class", "box");
+            container.appendChild(div);
+        }
+
+        box = document.querySelectorAll(".box")
+
+        for (let i = 0; i < box.length; i++) {
+            box[i].addEventListener("mouseover", () => {
+                box[i].style.backgroundColor = "blue";
+            });
+        }
+
+        let newWidth = newBoard * 50;
+
+        container.style.width = `${newWidth}px`;
     }
-
-    box = document.querySelectorAll(".box")
-
-    for (let i = 0; i < box.length; i++) {
-        box[i].addEventListener("mouseover", () => {
-            box[i].style.backgroundColor = "blue";
-        });
-    }
-
-    let newWidth = newBoard * 50;
-
-    container.style.width = `${newWidth}px`;
 });
 
 
