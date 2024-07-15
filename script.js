a8da6bae0de3c67632e60a4d1c7ee8fcb5358646
HolyMikeB <bnkrdicy@aol.com>
2024-07-14 22:09:58 -0500

    added reset function

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 const container = document.querySelector("#container");
 
+function addListener(box) {
+    for (let i = 0; i < box.length; i++) {
+        box[i].addEventListener("mouseover", () => {
+            box[i].style.backgroundColor = "blue";
+        });
+    }
+}
+
 for (let i = 0; i < 256; i++) {
     const div = document.createElement("div");
     div.setAttribute("class", "box");
@@ -13,3 +21,33 @@ for (let i = 0; i < box.length; i++) {
         box[i].style.backgroundColor = "blue";
     });
 }
+
+const reset = document.querySelector("button");
+
+reset.addEventListener("click", () => {
+    for (let i = 0; i < box.length; i++) {
+        container.removeChild(box[i]);
+    }
+    let newBoard = Number(prompt("Please enter the width of your new board (between 1 and 100)"));
+    let newBoardSize = newBoard ** 2;
+
+    for (let i = 0; i < newBoardSize; i++) {
+        const div = document.createElement("div");
+        div.setAttribute("class", "box");
+        container.appendChild(div);
+    }
+
+    box = document.querySelectorAll(".box")
+
+    for (let i = 0; i < box.length; i++) {
+        box[i].addEventListener("mouseover", () => {
+            box[i].style.backgroundColor = "blue";
+        });
+    }
+
+    let newWidth = newBoard * 50;
+
+    container.style.width = `${newWidth}px`;
+});
+
+
